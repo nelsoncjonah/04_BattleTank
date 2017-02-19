cd034eeaaeb98c8a6cb36b49bc21c56a1d7d7482
Jonah Nelson <jonah.nelson@hotmail.com>
2017-02-19 14:40:30 -0800

    147. ApplyForceAtLocation() in Action Created a TankTrack c++ class and wired it up to the xbox controller

--- BattleTank/Source/BattleTank/Private/Projectile.cpp
+++ BattleTank/Source/BattleTank/Private/Projectile.cpp
@@ -32,7 +32,6 @@ void AProjectile::Tick( float DeltaTime )
 void AProjectile::LaunchProjectile(float Speed)
 {
 
-	UE_LOG(LogTemp, Warning, TEXT("Fired at %f"), Speed);
 	ProjectileMovement->SetVelocityInLocalSpace(FVector::ForwardVector * Speed);
 	ProjectileMovement->Activate();
 }
